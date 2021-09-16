179acf50c08e81b0ec5316c4987bb74b86295a57
YourDad07 <salmanadhikari415@gmail.com>
2021-09-16 14:58:05 +0530

    Added env

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 os.environ['TEST']='TESTING'
-# load_dotenv()
+load_dotenv()
 user_name=os.environ.get('TOKEN')
 print(user_name)
 print(os.environ['TEST'])
-# a file named "geek", will be opened with the reading mode.
-# file = open('test1.txt', 'r')
-# This will print every line one by one in the file
-
-# Python code to illustrate split() function
-with open("test1.txt", "r") as file:
-    data = file.readlines()
-    str1=data[0]
-    str2=data[1]
-    print (str1,str2)
